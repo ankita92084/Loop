640a65bb6d7e6568c2e434613da3cfc2d4653735
Ankita <89608003+ankita92084@users.noreply.github.com>
2021-12-06 15:37:38 +0530

    Add files via upload

---
+++ table2to10.py
@@ -0,0 +1,11 @@
+num=int(input("enter the number"))
+n=int(input("enter the number"))
+
+while num<=n:
+    i=1
+    while i<=10:
+
+        print(num,"x",i,"=",num*i)
+        i=i+1
+    print()
+    num=num+1
